b8d81b6344db9247e32a2de1c68fd185692e34c8
aadigithub <41446647+aadigithub@users.noreply.github.com>
2021-03-21 22:41:44 +0530

    Add files via upload

---
+++ CascadeDropdownlist.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CascadeDropdownlist.aspx.cs" Inherits="GridViewWebApp.CascadeDropdownlist" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="ContinentLabel" runat="server" Font-Bold="True" Font-Size="XX-Large" Text="Continents"></asp:Label>
+&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="ContinentsDropDownList" runat="server"  
+                DataTextField="ContinentName" DataValueField="ContinentId" Font-Size="XX-Large" Width="379px" OnSelectedIndexChanged="ContinentsDropDownList_SelectedIndexChanged" AutoPostBack="True">
+            </asp:DropDownList>
+
+            <br />
+            <br />
+            <br />
+
+            <asp:Label ID="CountriesLabel" runat="server" DataTextField="CountryName" DataValueField="CountryId" Font-Bold="True" Font-Size="XX-Large" Text="Countries"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="CountriesDropDownList" DataTextField="CountryName" DataValueField="CountryId" AutoPostBack="true"
+                runat="server" Font-Size="XX-Large" Width="302px" OnSelectedIndexChanged="CountriesDropDownList_SelectedIndexChanged" >
+            </asp:DropDownList>
+
+            <br />
+            <br />
+            <br />
+
+            <asp:Label ID="CitiesLabel" runat="server" Font-Bold="True" Font-Size="XX-Large" Text="Cities"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="CitiesDropDownList" runat="server" DataTextField="CityName" DataValueField="CityId" Font-Size="XX-Large" Width="309px">
+            </asp:DropDownList>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CascadeDropdownlist.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace GridViewWebApp
+{
+    public partial class CascadeDropdownlist : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                PopulateContinentsDropDownList();
+            }
+        }
+
+        private void PopulateContinentsDropDownList()
+        {
+            ContinentsDropDownList.DataSource = GetData("spGetContinents", null);
+            ContinentsDropDownList.DataBind();
+
+            ListItem liContinent = new ListItem("Select Continent", "-1");
+            ContinentsDropDownList.Items.Insert(0, liContinent);
+
+            ListItem liCountries = new ListItem("Select Countries", "-1");
+            CountriesDropDownList.Items.Insert(0, liCountries);
+
+            ListItem liCities = new ListItem("Select Cities", "-1");
+            CitiesDropDownList.Items.Insert(0, liCities);
+
+            CountriesDropDownList.Enabled = false;
+            CitiesDropDownList.Enabled = false;
+        }
+        private DataSet GetData(string SPName,SqlParameter spparameter)
+        {
+          string cs =  ConfigurationManager.ConnectionStrings["DBConnect"].ConnectionString;
+            SqlConnection con = new SqlConnection(cs);
+            SqlDataAdapter da = new SqlDataAdapter(SPName,con);
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
+            if (spparameter != null)
+            {
+                da.SelectCommand.Parameters.Add(spparameter);
+            }
+
+
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            return ds;
+
+        }
+
+        protected void ContinentsDropDownList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(ContinentsDropDownList.SelectedIndex == 0)
+            {
+                //CountriesDropDownList.SelectedIndex = 0;
+                CountriesDropDownList.Enabled = false;
+                // CitiesDropDownList.SelectedIndex = 0;
+                CitiesDropDownList.Enabled = false;
+            }
+            else
+            {
+                CountriesDropDownList.Enabled = true;
+                SqlParameter parameter = new SqlParameter("@ContinentId", ContinentsDropDownList.SelectedValue);
+                DataSet dss = GetData("spGetCountriesByContinentId",parameter);
+                CountriesDropDownList.DataSource = dss;
+                CountriesDropDownList.DataBind();
+
+
+                ListItem liCountries = new ListItem("Select Countries", "-1");
+                CountriesDropDownList.Items.Insert(0, liCountries);
+
+                CitiesDropDownList.SelectedIndex = 0;
+                CitiesDropDownList.Enabled = false;
+            }
+        }
+
+        protected void CountriesDropDownList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (CountriesDropDownList.SelectedIndex == 0)
+            {
+                CitiesDropDownList.SelectedIndex = 0;
+                CitiesDropDownList.Enabled = false;
+            }
+            else
+            {
+                CitiesDropDownList.Enabled = true;
+                SqlParameter parameter = new SqlParameter("@CountryId", CountriesDropDownList.SelectedValue);
+                DataSet dss = GetData("spGetCitiesByCountryId", parameter);
+                CitiesDropDownList.DataSource = dss;
+                CitiesDropDownList.DataBind();
+                ListItem liCities = new ListItem("Select Cities", "-1");
+                CitiesDropDownList.Items.Insert(0, liCities);
+            }
+        }
+
+    }
+}
